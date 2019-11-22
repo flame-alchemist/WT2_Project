44b663fb3ea6eb10fce04547ada4c6dde83dfb45
flame-alchemist <sharathmenon10@gmail.com>
2019-11-22 17:00:12 +0530

    backend updates

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask_cors import CORS
 from bson.json_util import dumps
 import json, base64
 import requests,json
-
+import uuid
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -15,7 +15,7 @@ app.config['MYSQL_DB'] = 'project'
 
 mysql = MySQL(app)
 
-@app.route('/api/v1/uadd_user', methods=['POST'])
+@app.route('/api/v1/add_user', methods=['POST'])
 def add_user():
 
     cur = mysql.connection.cursor()
@@ -24,7 +24,8 @@ def add_user():
     email = request.json["email"]
     password = request.json["password"]
     password = base64.b64encode(password.encode("utf-8"))
-    row = (int('1'), name, email, password)
+    user_id = str(uuid.uuid1())
+    row = (user_id, name, email, password)
     print(row)
     cur.execute("insert into user values ( %s, %s, %s, %s)",row)
     mysql.connection.commit()
@@ -32,16 +33,9 @@ def add_user():
 
     return jsonify({}),201
 
-    # res = users.find_one({'email':email})
-
-    # if res:
-    #     abort(405)
-    # else:
-    #     users.insert({"name":name, "id":email, "phno":phno, "university":university, "CGPA":cgpa})
-    #     return jsonify({}),201
-@app.route('/api/v1/check_user', methods=['POST'])
+@app.route('/api/v1/checkUser', methods=['POST'])
 def check_user():
-
+    # print("check user")
     cur = mysql.connection.cursor()
 
     email = request.json["email"]
@@ -49,30 +43,50 @@ def check_user():
     password = base64.b64encode(password.encode("utf-8"))
     row = (email, password)
     print(row)
-    data = cur.execute("select username from user where email=%s and password=%s",row)
+    cur.execute("select userID,username from user where email=%s and password=%s",row)
+    data = cur.fetchall()
+    print(data)
     cur.close()
 
     if(data):
         print('success')
-        return render_template(‘project.html’)
+        return_data = {"id":str(data[0][0]), "name":str(data[0][1])}
+        print("return_data", return_data)
+        return jsonify(return_data),201
     else:
         print('failure')
-        return 403
+        return jsonify({}),403
 
 
 @app.route('/api/v1/getBooks', methods=['POST'])
 def get_books():
-    
-#
-# @app.route('/')#, methods=['GET', 'POST'])
-# def queries():
-#     cur = mysql.connection.cursor()
-#     data = cur.execute('select * from books')
-#     cur.close()
-#     if data:
-#         return "success"
-#     else:
-#         return "failure"
+    chunk_count = int(request.json['chunk_count'])
+    cur = mysql.connection.cursor()
+    cur.execute('select original_title, authors, image_url from books where id between ' + str(chunk_count*6) + ' and ' + str((chunk_count*6+6)))
+    # cur.execute('select original_title, authors from books where id between ' + str(chunk_count) + ' and ' + str((chunk_count*10+10)))
+    data = cur.fetchall()
+    cur.close()
+    print(data)
+    return_data = []
+    if data:
+    for i in data:
+    tmp = {"title":i[0],"author":i[1],"url":i[2]}
+    return_data.append(tmp)
+    return jsonify({"data":return_data}),200
+    else:
+    return jsonify({}),400
+
+
+@app.route('/api/v1/suggest', methods=['POST'])
+def get_terms():
+    term = request.json['search_term']
+    res = []
+    cur = mysql.connection.cursor()
+    cur.execute("select original_title from books where original_title like '" + term + "%'");
+    data = cur.fetchall()
+    print(data)
+    return_data = [element for t in data for element in t]
+    return jsonify({"data":return_data}), 200;
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8080, debug=True)
